dc3b75b5781b8739799d80fa706f5ea323e258e9
Gumbo64 <35547211+Gumbo64@users.noreply.github.com>
2025-11-22 23:52:53 +1100

    preparing for blockchain init screen

--- frontend/src/context/BlockchainContext.jsx
+++ frontend/src/context/BlockchainContext.jsx
@@ -2,30 +2,31 @@ import React, { createContext, useState, useEffect } from 'react';
 import { getROM } from "sm64-binds-frontend";
 import { BlockChainClientWeb } from "sm64-crypto-browser";
 
-async function init_blockchain(name, ticket) {
-    let rom_bytes = new Uint8Array(await getROM());
-    return await BlockChainClientWeb.new(rom_bytes, name, ticket);
-}
-
 const BlockchainContext = createContext();
 
 const BlockchainProvider = ({ children }) => {
 	const [hasRom, setHasRom] = useState(false);
 	const [blockchain, setBlockchain] = useState(null);
 
-	useEffect(() => {
-		const fn = async () => {
-			if (hasRom && blockchain == null) {
-				const name = new URLSearchParams(window.location.search).get('name') || prompt("Enter your username:");
-				const ticket = new URLSearchParams(window.location.search).get('ticket') || prompt("Enter your ticket (or otherwise empty)");
-				setBlockchain(await init_blockchain(name, ticket));
-			}
+	const init_blockchain = async (name, ticket) => {
+		if (blockchain != null) {
+			console.log("Reinitialising blockchain!")
 		}
-		fn();
+
+		let rom_bytes = new Uint8Array(await getROM());
+		setBlockchain(await BlockChainClientWeb.new(rom_bytes, name, ticket));
+	}
+
+	useEffect(() => {
+		if (hasRom && blockchain == null) {
+			const name = new URLSearchParams(window.location.search).get('name') || prompt("Enter your username:");
+			const ticket = new URLSearchParams(window.location.search).get('ticket') || prompt("Enter your ticket (or otherwise empty)");
+			init_blockchain(name, ticket);
+		}
 	}, [hasRom]);
 
 	return (
-		<BlockchainContext.Provider value={{ hasRom, setHasRom, blockchain }}>
+		<BlockchainContext.Provider value={{ hasRom, setHasRom, blockchain, init_blockchain }}>
 		{children}
 		</BlockchainContext.Provider>
 	);
